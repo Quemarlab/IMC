73326b2f173892884f3abd3a803f579ced375f1b
Quemarlab <usabuwerakendlyquemar@gmail.com>
2024-06-17 10:32:16 +0200

    commit

--- ContolPanel/DB/smc.sql
+++ ContolPanel/DB/smc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 11, 2024 at 08:37 PM
+-- Generation Time: Jun 14, 2024 at 06:41 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -61,7 +61,11 @@ INSERT INTO `access_logs` (`id`, `account_id`, `ip_address`, `country`, `region`
 (15, 'SFRACC02932', '41.216.108.175', 'Rwanda', 'Kigali City', 'Kigali', '', '', '2024-06-10 16:18:17'),
 (0, 'SFRACC02932', '196.12.150.183', 'Rwanda', 'Southern Province', 'Runda', '', '', '2024-06-11 15:05:31'),
 (0, 'SFRACC02932', '41.216.116.237', 'Rwanda', 'Kigali City', 'Kigali', '', '', '2024-06-11 16:09:03'),
-(0, 'SFRACC02932', '41.216.116.237', 'Rwanda', 'Kigali City', 'Kigali', '', '', '2024-06-11 16:14:15');
+(0, 'SFRACC02932', '41.216.116.237', 'Rwanda', 'Kigali City', 'Kigali', '', '', '2024-06-11 16:14:15'),
+(0, 'SFRACC02932', '196.12.138.148', 'Rwanda', 'Southern Province', 'Runda', '', '', '2024-06-14 07:59:38'),
+(0, 'SFRACC02932', '196.12.138.148', 'Rwanda', 'Southern Province', 'Runda', '', '', '2024-06-14 10:24:23'),
+(0, 'SFRACC02932', '41.216.106.176', 'Rwanda', 'Kigali City', 'Kigali', '', '', '2024-06-14 12:57:02'),
+(0, 'SFRACC02932', '41.216.106.176', 'Rwanda', 'Kigali City', 'Kigali', '', '', '2024-06-14 14:44:07');
 
 -- --------------------------------------------------------
 
@@ -97,6 +101,30 @@ INSERT INTO `account_holders` (`id`, `account_id`, `firstname`, `lastname`, `ema
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `file`
+--
+
+CREATE TABLE `file` (
+  `id` int(11) NOT NULL,
+  `file_code` varchar(255) NOT NULL,
+  `file` varchar(255) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `file`
+--
+
+INSERT INTO `file` (`id`, `file_code`, `file`, `date`) VALUES
+(4, 'PR9189', 'FILE529_1.png', '2024-06-14 13:03:39'),
+(5, 'PR6462', 'FILE5853_1.png', '2024-06-14 13:12:05'),
+(6, 'PR3197', 'FILE9792_Screenshot 2024-06-09 115946.png', '2024-06-14 14:26:22'),
+(7, 'PR819', 'FILE566_Screenshot 2024-06-12 140751.png', '2024-06-14 14:33:02'),
+(8, 'PR67', 'FILE3405_1.png', '2024-06-14 15:00:41');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `project`
 --
 
@@ -105,13 +133,29 @@ CREATE TABLE `project` (
   `code` varchar(255) NOT NULL,
   `title` varchar(255) NOT NULL,
   `content` longtext NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `image` varchar(255) NOT NULL
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `project`
+--
+
+INSERT INTO `project` (`id`, `code`, `title`, `content`, `date`) VALUES
+(4, 'PR9189', 'Welcome to the store', 'sdjhkl hgjhbjhb&nbsp;', '2024-06-14 13:03:39'),
+(5, 'PR6462', 'This is all american site', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mollis nisi sit amet elit vestibulum fermentum. Sed vestibulum congue neque, finibus viverra dui volutpat vitae. Suspendisse ut posuere mi, sit amet porttitor velit. In lectus ipsum, posuere vitae auctor non, fermentum at nulla. Donec ut dignissim dolor. Donec consectetur aliquet luctus. Mauris porta in velit sit amet aliquet. Vivamus et neque mi. Nullam leo arcu, placerat sed faucibus ac, pellentesque non tortor.', '2024-06-14 13:12:05'),
+(6, 'PR3197', 'Bertin is the one who is organising ', 'Something is the data based below', '2024-06-14 14:26:22'),
+(7, 'PR819', 'Testing the editor', '&lt;p&gt;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mollis nisi sit amet elit vestibulum fermentum. Sed vestibulum congue neque, finibus viverra dui volutpat vitae. Suspendisse ut posuere mi, sit amet porttitor velit. In lectus ipsum, posuere vitae auctor non, fermentum at nulla. Donec ut dignissim dolor. Donec consectetur aliquet luctus. Mauris porta in velit sit amet aliquet. Vivamus et neque mi. Nullam leo arcu, placerat sed faucibus ac, pellentesque non tortor.&lt;/p&gt;', '2024-06-14 14:33:02'),
+(8, 'PR67', 'My Milestone is amazing and so gorgeous', '&lt;h1&gt;My milestone is so amazing and so gorgeous&lt;/h1&gt;', '2024-06-14 15:00:41');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `file`
+--
+ALTER TABLE `file`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `project`
@@ -124,10 +168,16 @@ ALTER TABLE `project`
 --
 
 --
+-- AUTO_INCREMENT for table `file`
+--
+ALTER TABLE `file`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT for table `project`
 --
 ALTER TABLE `project`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
